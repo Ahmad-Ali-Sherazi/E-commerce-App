f00da8ad4b90d96b3777565e082c32a95c755d14
ahmad_ali_sherazi <Ahmad-Ali-Sherazi@users.noreply.github.com>
2021-04-23 02:58:02 +0500

    DB connection initial with express and MANGODB

--- Backend/app.js
+++ Backend/app.js
@@ -3,11 +3,25 @@ const express = require('express');
 const app = express();
 
 require('dotenv/config');
+const mongoose= require('mongoose');
+const link=process.env.API_URL;
+const DB=process.env.DB_CONNECTION;
 
-link=process.env.API_URL;
-app.get('/', (req, res)=> {
+app.get(link+'app', (req, res)=> {
     res.send('Message from API');
 })
+
+mongoose.connect(DB, {
+    useNewUrlParser:true,
+    useUnifiedToplogy:true,
+    dbName:'Eshop'
+})
+.then(()=>{
+    console.log('DB is connected');
+})
+.catch((err)=>{
+    console.log(err);
+})
 app.listen(3000, ()=> {
-    console.log(link);
+    console.log(link+'app');
 })
